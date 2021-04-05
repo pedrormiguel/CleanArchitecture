7a603e263251ba4e52fd6ee532dd77f814c4e590
Pedro Ruiz <pedrormiguel@outlook.es>
2021-04-05 18:57:41 -0400

    Handler Finish

--- Application/Features/Events/GetEventsListQueryHandler.cs
+++ Application/Features/Events/GetEventsListQueryHandler.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using Application.Persistence.Contracts;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
 namespace Application.Features.Events
 {
-    public class GetEventsListQueryHandler
+    //It's the Handler of the Message, whose know how it has to interact each other
+    public class GetEventsListQueryHandler : IRequestHandler<GetEventsListQuerys, List<EventListVm>>
     {
-        public GetEventsListQueryHandler()
+        private readonly IAsyncRepository<Event> _eventRespository;
+        private readonly IMapper _autoMapper;
+
+        public GetEventsListQueryHandler(IAsyncRepository<Event> eventRespository, IMapper autoMapper)
         {
+            _eventRespository = eventRespository;
+            _autoMapper = autoMapper;
+        }
+
+        public async Task<List<EventListVm>> Handle(GetEventsListQuerys request, CancellationToken cancellationToken)
+        {
+            //Getting the Data form the Store
+            var eventAll = (await _eventRespository.ListAllAsync()).OrderBy(x => x.Date);
+
+            //Mapping the Raw Data in something that is useful and limited. ( Do not expose our desing table )
+            return _autoMapper.Map<List<EventListVm>>(eventAll);
         }
     }
 }
