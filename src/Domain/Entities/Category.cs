d1179702200abf45fc85e9da0ff75fbcc2404b6b
Pedro Ruiz <pedrormiguel@outlook.es>
2021-04-06 19:18:25 -0400

    Added Entities

---
+++ src/Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
