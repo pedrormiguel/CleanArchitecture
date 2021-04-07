f55b77014187724b42b8c83ce75bb14f38bfdec5
Pedro Ruiz <pedrormiguel@outlook.es>
2021-04-06 20:39:11 -0400

    AddedProfileMapping

---
+++ src/CORE/Application/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Events;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventListVm>().ReverseMap();
+        }
+    }
+}
