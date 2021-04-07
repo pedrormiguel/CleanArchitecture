08a0eb7f21786aecad5f13c4b769e800b7022b69
Pedro Ruiz <pedrormiguel@outlook.es>
2021-04-06 20:26:01 -0400

    Added Profile To Map

--- src/CORE/Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/CORE/Application/Contracts/Persistence/IAsyncRepository.cs
@@ -7,7 +7,11 @@ namespace Application.Contracts.Persistence
     public interface IAsyncRepository<T> where T : class
     {
         Task<T> GetByIdAsync(Guid Id);
+
+        // --------------------------
         Task<IReadOnlyList<T>> ListAllAsync();
+        //--------------------------
+
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
         Task<T> DeleteAsync(T entity);
