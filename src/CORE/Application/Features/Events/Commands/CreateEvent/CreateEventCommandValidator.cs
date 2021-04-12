5ba5ee1457944884874d6e3a3eaa937595d7a072
Pedro Ruiz <pedrormiguel@outlook.es>
2021-04-12 19:18:58 -0400

    Added Validation for EventNameAndDateUnique

--- src/CORE/Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
+++ src/CORE/Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Transactions;
+using Application.Contracts.Persistence;
 using FluentValidation;
 
 namespace Application.Features.Events.Commands.CreateEvent
 {
     public class CreateEventCommandValidator : AbstractValidator<EventCreateVm>
     {
-        public CreateEventCommandValidator()
+        private readonly IEventRepository _eventRepository;
+        public CreateEventCommandValidator(IEventRepository eventRepository)
         {
+            _eventRepository = eventRepository;
+            
             RuleFor(e => e.Name)
                 .NotEmpty().WithMessage("{PropertyName} is required.")
                 .NotNull()
@@ -19,10 +25,19 @@ namespace Application.Features.Events.Commands.CreateEvent
                 .NotNull()
                 .GreaterThan(DateTime.Now);
 
+            RuleFor(e => e)
+                .MustAsync(EventNameAndDateUnique)
+                .WithMessage("An event with the same name and data already exists");
+
             RuleFor(e => e.Price)
                 .NotEmpty().WithMessage("{PropertyName} is required")
                 .NotNull()
                 .GreaterThan(0);
+        }
+
+        private async Task<bool> EventNameAndDateUnique(EventCreateVm e , CancellationToken cancellationToken)
+        {
+            return !( await _eventRepository.IsEventNameAndDateUnique(e.Name, e.Date) );
         }
     }
 }
