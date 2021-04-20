1024e1be478c41ca3b3f92c9a8b3d18a1ae2816b
Pedro Ruiz <pedrormiguel@outlook.es>
2021-04-20 03:26:26 -0400

    Added to the HandlerEvent the send Email

--- src/CORE/Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ src/CORE/Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Contracts.Infrastructure;
 using Application.Contracts.Persistence;
 using Application.Exceptions;
+using Application.Models.Mail;
 using AutoMapper;
 using Domain.Entities;
 using MediatR;
@@ -13,11 +15,13 @@ namespace Application.Features.Events.Commands.CreateEvent
     {
         private readonly IEventRepository _eventRepository;
         private readonly IMapper _autoMapper;
+        private readonly IEmailServices _emailServices;
 
-        public CreateEventCommandHandler(IEventRepository eventRepository, IMapper autoMapper)
+        public CreateEventCommandHandler(IEventRepository eventRepository, IMapper autoMapper, IEmailServices emailServices)
         {
             _eventRepository = eventRepository;
             _autoMapper = autoMapper;
+            _emailServices = emailServices;
         }
 
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
@@ -31,6 +35,22 @@ namespace Application.Features.Events.Commands.CreateEvent
             var eventMapped = _autoMapper.Map<Event>(request);
 
             await _eventRepository.AddAsync(eventMapped);
+
+            var email = new Email
+            {
+                To = "pedrormiguel@outlook.es", 
+                Subject = $"An new Event was created: {request.Name}", 
+                Body = $"Hi, It's a new Event {request}"
+            };
+            
+            try
+            {
+                await _emailServices.SendEmail(email);
+            }
+            catch(Exception e)
+            { 
+                //TODO this shouldn't stop the API from doing else so this can be logged 
+            }
 
             return eventMapped.EventId;
         }
