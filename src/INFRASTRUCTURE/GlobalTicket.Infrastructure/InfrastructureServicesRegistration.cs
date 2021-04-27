1873a12bb56d953760b3593071bca124bb1a7a86
Pedro Ruiz <pedrormiguel@outlook.es>
2021-04-27 18:52:02 -0400

    Fix

---
+++ src/INFRASTRUCTURE/GlobalTicket.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,20 @@
+using Application.Contracts.Infrastructure;
+using Application.Models.Mail;
+using GlobalTicket.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GlobalTicket.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            serviceCollection.AddTransient<IEmailServices, EmailServices>();
+
+            return serviceCollection;
+        }
+    }
+}
