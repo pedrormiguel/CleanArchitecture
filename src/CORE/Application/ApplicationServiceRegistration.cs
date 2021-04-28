f8959d8e381e66e3602e412e954a867ff20cd461
Pedro Ruiz <pedrormiguel@outlook.es>
2021-04-28 18:11:54 -0400

    Renamed ApplicationServiceRegistration Layer

--- src/CORE/Application/ApplicationServiceRegistration.cs
+++ src/CORE/Application/ApplicationServiceRegistration.cs
@@ -4,14 +4,14 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Application
 {
-    public static class ApplicationServiceResgistrarion
+    public static class ApplicationServiceRegistration
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection service)
         {
             service.AddAutoMapper(Assembly.GetExecutingAssembly());
             service.AddMediatR(Assembly.GetExecutingAssembly());
-
-
+            
+            
             return service;
         }
     }
