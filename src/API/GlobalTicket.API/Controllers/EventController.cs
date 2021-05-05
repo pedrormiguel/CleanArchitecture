dff3f1f2f2a97795528d6dafec35a0929b14008e
Pedro Ruiz <pedrormiguel@outlook.es>
2021-05-05 03:40:29 -0400

    Added Controller for Event

---
+++ src/API/GlobalTicket.API/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalTicket.API.Controllers
+{
+    public class EventController : ControllerBase
+    {
+        public EventController()
+        {
+        }
+    }
+}
