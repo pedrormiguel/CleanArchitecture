145e3d3e0fa9051ce077d30c787969556bcd98dd
Pedro Ruiz <pedrormiguel@outlook.es>
2021-05-07 18:11:06 -0400

    Added Services & EndPoint for Swagger

--- src/API/GlobalTicket.API/Startup.cs
+++ src/API/GlobalTicket.API/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application;
 using GlobalTicket.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Persistence;
 
 namespace GlobalTicket.API
@@ -29,16 +25,22 @@ namespace GlobalTicket.API
         {
             services.AddApplicationServices();
             services.AddInfrastructureService(_configuration); //TODO add appsetting.json ( EmailSettings )
-            services.AddPersistenceServices(_configuration);  //TODO add appsetting.json  ( ConnectionString - GlobalTicketManagementStringConnection )
-
+            services.AddPersistenceServices(_configuration); //TODO add appsetting.json  ( ConnectionString - GlobalTicketManagementStringConnection )
 
             services.AddControllers();
-
             services.AddCors(opt =>
             {
-
                 opt.AddPolicy("open", builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            });
 
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "GlobalTicket API",
+                    Version = "v1"
+                });
             });
         }
 
@@ -48,17 +50,16 @@ namespace GlobalTicket.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(
+                    c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "GlobalTicket API"); }
+                );
             }
 
             app.UseHttpsRedirection();
             app.UseRouting();
-
             app.UseCors("open");
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
